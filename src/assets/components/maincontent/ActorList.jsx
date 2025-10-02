c3bb36f4f46cfcaeb7a89774f94d36acd1761b07
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-10-02 15:29:52 +0200

    finalizzazione milestone 1

--- src/assets/components/maincontent/ActorList.jsx
+++ src/assets/components/maincontent/ActorList.jsx
@@ -1,5 +1,24 @@
+import CardActor from "./CardActor";
+import { useState, useEffect } from "react";
+
 function ActorList() {
   
+    const [actors, setActors] = useState([]);
+
+    function fetchActors() {
+        axios.get("https://lanciweb.github.io/demo/api/actors/")
+            .then((res) => {
+                console.log(res.data);
+                setActors(res.data);
+            })
+            .catch(error => console.log(error)
+            )
+    }
+
+    useEffect(() => {
+        fetchActors()
+    }
+        , [])
 
   return (
     <>
